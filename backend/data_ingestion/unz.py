f23582d58b2df69add1055d2ce5aaeb950ae00fe
phat <ngo>
2025-03-22 19:56:27 -0400

    Added some front end files and also additional details

--- backend/data_ingestion/unz.py
+++ backend/data_ingestion/unz.py
@@ -1,6 +1,7 @@
 import os
 import zipfile
 
+
 def extract_all_zip_files(directory):
     """
     Extracts all zip files in the specified directory and its subdirectories.
@@ -11,15 +12,16 @@ def extract_all_zip_files(directory):
             if file.endswith(".zip"):
                 zip_path = os.path.join(root, file)
                 extract_path = os.path.join(root, os.path.splitext(file)[0])
-                
+
                 with zipfile.ZipFile(zip_path, 'r') as zip_ref:
                     zip_ref.extractall(extract_path)
-                
+
                 print(f"Extracted {zip_path} to {extract_path}")
+
 
 if __name__ == "__main__":
     # Define the directory containing the zip files
     bills_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "data", "bills")
-    
+
     # Extract all zip files in the directory
     extract_all_zip_files(bills_directory)
